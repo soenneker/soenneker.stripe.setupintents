49073133a70890071409d090000a13dc3356878a
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:50 -0500

    xml commenting

--- src/Soenneker.Stripe.SetupIntents/StripeSetupIntentsUtil.cs
+++ src/Soenneker.Stripe.SetupIntents/StripeSetupIntentsUtil.cs
@@ -129,11 +129,18 @@ public sealed class StripeSetupIntentsUtil : IStripeSetupIntentsUtil
                             .NoSync();
     }
 
+    /// <summary>
+    /// Releases resources used by the current instance.
+    /// </summary>
     public void Dispose()
     {
         _service.Dispose();
     }
 
+    /// <summary>
+    /// Asynchronously releases resources used by the current instance.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public ValueTask DisposeAsync()
     {
         return _service.DisposeAsync();
